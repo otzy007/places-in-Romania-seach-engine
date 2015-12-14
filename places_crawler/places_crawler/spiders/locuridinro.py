1bc9e9a20fce7a041345c9435cd195adf14ca14d
Andrei Gliga <otzy_007@yahoo.com>
2015-12-14 14:58:47 +0200

    crawl the image from the article

--- places_crawler/places_crawler/spiders/locuridinro.py
+++ places_crawler/places_crawler/spiders/locuridinro.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import logging
 from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
 from places_crawler.items import PlacesCrawlerItem
 
+logger = logging.getLogger('mycustomlogger')
 
 class LocuridinroSpider(scrapy.Spider):
     name = "locuridinro"
     allowed_domains = ["locuridinromania.ro"]
     start_urls = (
-        u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-alba', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-arad', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-arges', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-bacau', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-bihor', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-bistrita-nasaud', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-botosani', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-braila', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-brasov', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-bucuresti', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-buzau', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-calarasi', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-caras-severin', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-cluj', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-constanta', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-covasna', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuir-din-judetul-dambovita', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-dolj', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-galati', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-giurgiu', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-gorj', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-harghita', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-hunedoara', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-ialomita', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-iasi', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-ilfov', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-maramures', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-mehedinti', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuir-din-judetul-mures', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-neamt', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-olt', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-prahova', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-salaj', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-satu-mare', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-sibiu', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-suceava', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-teleorman', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-timis', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-tulcea', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-valcea', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-vaslui', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-vrancea'
+        u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-alba', u'http://www.locuridinromania.ro/categorie/locuri-de-vizitat-in-romania/locuri-din-judetul-arad'
     )
 
     def parse(self, response):
@@ -19,7 +21,30 @@ class LocuridinroSpider(scrapy.Spider):
             item = PlacesCrawlerItem()
             name = sel.xpath('header/a/@title').extract()
             item['name'] = name[0] if name else ""
-            item['link'] = sel.xpath('header/a/@href').extract()
+
+            link = sel.xpath('header/a/@href').extract()
+            item['link'] = link
             item['short_decription'] = sel.xpath('div[@class="entry-content"]').extract()
 
-            yield item
+            request = scrapy.Request(link[0], callback=self.parse_full_page)
+            request.meta['item'] = item
+
+            #
+            yield request
+
+    def parse_full_page(self, response):
+        item = response.meta['item']
+        logger.info('Visited %s', response.url)
+
+        responseSelector = Selector(response)
+        sel = responseSelector.xpath('//article')
+        item['image'] = sel.xpath('div[@class="entry-content"]/h3/img/@data-lazy-src').extract()
+
+        if item['image'] == []:
+            item['image'] = sel.xpath('div[@class="entry-content"]/p/img/@src').extract()
+        if item['image'] == []:
+            item['image'] = sel.xpath('div[@class="entry-content"]/p/a/img/@data-lazy-src').extract()
+        if item['image'] == []:
+            item['image'] = sel.xpath('div[@class="entry-content"]/h3/a/img/@data-lazy-src').extract()
+
+        return item
