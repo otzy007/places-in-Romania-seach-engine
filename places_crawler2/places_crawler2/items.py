ec759b6fbda7bee0f5a07f4a724830cab82c7715
root <root@ionut.(none)>
2015-12-15 22:42:50 +0200

    places2

--- places_crawler2/places_crawler2/items.py
+++ places_crawler2/places_crawler2/items.py
@@ -13,4 +13,6 @@ class PlacesCrawler2Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     name = Field()
+    address = Field()
+    image = Field()
     short_decription = Field()
